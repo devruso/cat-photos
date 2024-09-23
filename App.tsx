9c61b0245263e2ae04dbaa914409133384e73d9c
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-09-23 13:04:04 -0300

    feature: app display routes

--- App.tsx
+++ App.tsx
@@ -1,35 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import Routes from './src/routes/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-      <View>
-        <Text>This is my first app</Text>
-      </View>
-      <TouchableOpacity style={styles.viewStyle} onPress={()=>console.log('hello world!')}>
-        <Text>Button</Text>
-      </TouchableOpacity>
-      <Button title='Very nice' />
-    </View>
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  viewStyle:{
-    backgroundColor: 'red',
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    width: 100,
-    height: 50,
-  }
-});
