dba9d8c4b77396d59136e310308a5244af20e4e8
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-09-26 16:19:55 -0300

    build: add dependencies, types and theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
-import Routes from './src/routes/index';
+import Routes from "./src/routes/index";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { ThemeProvider } from "styled-components/native";
+import light from "./src/themes/light";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    <Routes/>
+    <GestureHandlerRootView>
+      <StatusBar style="dark" />
+      <ThemeProvider theme={light}>
+        <Routes />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ src/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    // tokens de cores
+    COLORS: {
+        BACKGROUND: "#F8F9FA",
+        PRIMARY1: "#E9ECEF",
+        PRIMARY2: "#DEE2E6",
+        SECONDARY1: "#8f94A3",
+        SECONDARY2: '#343A40',
+        DARK1: '#212529',
+      }
+};
