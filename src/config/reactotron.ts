7bb81299d50c28b5fbcfacbd7c5cae4ab6707e4e
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-10-07 14:25:34 -0300

    feature: create reactotron config file

---
+++ src/config/reactotron.ts
@@ -0,0 +1,13 @@
+import { NativeModules } from 'react-native';
+import reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const { scriptURL } = NativeModules.SourceCode;
+const hostName = scriptURL.split('://')[1].split(':')[0];
+
+reactotron.setAsyncStorageHandler!!(AsyncStorage)
+  .configure({ host: hostName })
+  .useReactNative()
+  .clear();
+
+export default reactotron;
