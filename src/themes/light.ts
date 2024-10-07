496a6bd016fecba29f7cd873eedf767c47e929d7
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-10-07 14:37:17 -0300

    feature: add label colors to theme

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -7,5 +7,11 @@ export default {
         SECONDARY1: "#8f94A3",
         SECONDARY2: '#343A40',
         DARK1: '#212529',
+        LABEL1: '#DEC5E3',
+        LABEL2: '#FFD3B6',
+        LABEL3: '#B5EAD7',
+        LABEL4: '#FFB7B2',
+        LABEL6: '#81F7E5',
+        LABEL7: '#FFC300',
       }
 };
