1aa008c7004019a5e300d20c0489efd6564bf135
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-10-07 14:39:22 -0300

    feature: build and style of cat details screen

--- src/screens/CatDetails/index.tsx
+++ src/screens/CatDetails/index.tsx
@@ -1,17 +1,41 @@
 import Header from '../../components/Header';
-import { Container, Text, StyledImage} from './styles';
+import { Container, Content , Text, TextLabel,StyledImage, TextContainer, TextTitle, Separator} from './styles';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 
 export default function CatDetails({route} : CatDetailsProps) {
-  const { name, image, breed } = route.params || '';
+  const { image , id} = route.params;
+  const {name: breedName, description, temperament} = route.params.breed;
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+
+
+const temperamentArray = temperament.split(',');
 
   return (
     <Container>
       <Header onPress={() => navigation.navigate('Home')} />
-      <StyledImage source={{ uri: image }} />
-      <Text>{name}</Text>
-      <Text>{breed}</Text>
+        <Content
+          contentContainerStyle={{
+            alignItems: 'center',
+            justifyContent: 'center',
+
+          }}
+        >
+            <StyledImage source={{ uri: image }} />
+            <TextContainer>
+              <TextTitle>Breed: </TextTitle>
+              <TextTitle>{breedName}</TextTitle>
+            </TextContainer>
+            <Text>{description}</Text>
+            <TextContainer>
+            { temperamentArray.map((item, index) => {
+                return (<TextLabel index={index} key={item}>{item}</TextLabel>)
+              })
+            } 
+            <Separator 
+              style={{marginBottom: 20}}
+            />
+            </TextContainer>
+        </Content>
     </Container>
   );
 }
--- src/screens/CatDetails/styles.ts
+++ src/screens/CatDetails/styles.ts
@@ -2,18 +2,50 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: ${({theme}) => theme.COLORS.BACKGROUND1};
+  background-color: ${({theme}) => theme.COLORS.PRIMARY1};
   align-items: center;
   justify-content: center;
 `;
 
+export const Content = styled.ScrollView`
+  width: 100%;
+  flex: 1;
+`
+export const TextContainer = styled.View`
+  width: 80%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
 export const Text = styled.Text`
-    font-size: 24px;
+    font-size: 20px;
     color : ${({theme}) => theme.COLORS.DARK1};
+    margin-top: 16px;
+    padding: 4px;
+`
+export const TextTitle = styled.Text`
+    font-size: 24px;
+    color : ${({theme}) => theme.COLORS.DARK1};  margin-top: 16px;
+    text-align: justify;
+`
+
+export const TextLabel = styled.Text<{index : number}>`
+  color: ${({theme}) => theme.COLORS.DARK1};
+  font-size: 16px;
+  background-color: ${({theme, index}) =>  theme.COLORS[`LABEL${index % 7 + 1}`]};
+  border: 2px solid ${({theme, index}) =>  theme.COLORS[`LABEL${index % 7 + 1}`]};
+  border-radius: 8px;
+  margin: 4px;
+  padding: 1px;
 `
 
 export const StyledImage = styled.Image`
-  width: 200px;
-  height: 200px;
-  border-radius: 10px;
+  width: 100%;
+  height: 350px;
 `;
+
+export const Separator = styled.View`
+  height: 1px;
+  width: 100%;
+`
