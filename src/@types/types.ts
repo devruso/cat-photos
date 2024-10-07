2fe7bd3ef0bb91989804cc686f698731c1f24d10
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-10-07 14:25:04 -0300

    feature: add application types

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -1,16 +1,30 @@
-type Cat = {
+interface Cat {
+  id: string;
   name: string;
   image: string;
-  breed: string;
+  breed: {
+    name :string;
+    description: string;
+    temperament: string;
+  } 
 }
 
-type RootStackParamList = {
-    Home: undefined;
+interface Cats {
+  cats: Cat[];
+}
+
+interface RootStackParamList {
+     Home: undefined;
     'Cat Details': Cat;
 };
 
-type CatDetailsProps = {
+interface CatDetailsProps {
     route: {
       params: Cat;
     };
   };
+
+//Necessário criar e tipar o módulo do que tem no arquivo .env a ser importado
+declare module '@env' {
+  export const THE_CAT_API_KEY: string;
+}
