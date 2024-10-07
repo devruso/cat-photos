6ff8e76df915ce13a9c25769ef8e483e7dd6fcf8
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-10-07 14:23:46 -0300

    feature: implement reactotron

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
 import "react-native-gesture-handler";
-
 import Routes from "./src/routes/index";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { ThemeProvider } from "styled-components/native";
 import light from "./src/themes/light";
 import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import reactotron from "reactotron-react-native";
+
+
+if (__DEV__) {
+  reactotron.connect();
+}
 
 export default function App() {
+  const client = new QueryClient();
+  
   return (
     <GestureHandlerRootView>
-      <StatusBar style="dark" />
-      <ThemeProvider theme={light}>
-        <Routes />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <StatusBar style="dark" />
+        <ThemeProvider theme={light}>
+          <Routes />
+        </ThemeProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
